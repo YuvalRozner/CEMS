3d6a1b1e32dc4ee32cc6691a59f8e9a37a55809b
DorShabat <128496573+DorShabat@users.noreply.github.com>
2023-05-12 10:49:00 +0300

    trying to update server while update client

--- Code/src/server/CEMSserver.java
+++ Code/src/server/CEMSserver.java
@@ -1,25 +1,134 @@
-package CEMSserver;
+package server;
 
-public class CEMSserver {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
-	public void handleMessageFromClient() {
-		// TODO - implement CEMSserver.handleMessageFromClient
-		throw new UnsupportedOperationException();
-	}
+import ocsf.server.AbstractServer;
+import ocsf.server.ConnectionToClient;
 
-	public void serverStarted() {
-		// TODO - implement CEMSserver.serverStarted
-		throw new UnsupportedOperationException();
-	}
+//EchoServer
+public class CEMSserver extends AbstractServer 
+{
+  //Class variables *************************************************
+  
+  /**
+   * The default port to listen on.
+   */
+  //final public static int DEFAULT_PORT = 5555;
+  
+  //Constructors ****************************************************
+  
+  /**
+   * Constructs an instance of the echo server.
+   *
+   * @param port The port number to connect on.
+   * 
+   */
+ 
+ Connection conn;
+ 
+  public CEMSserver(int port) 
+  {
+    super(port);
+  }
 
-	public void serverStoped() {
-		// TODO - implement CEMSserver.serverStoped
-		throw new UnsupportedOperationException();
-	}
+  //Instance methods ************************************************
+  
+  /**
+   * This method handles any messages received from the client.
+   *
+   * @param msg The message received from the client.
+   * @param client The connection from which the message originated.
+   * @param 
+   */
+  public void handleMessageFromClient  (Object msg, ConnectionToClient client) {
+	  
+	  Statement stmt = null;
+	  ResultSet data;
+	  boolean flag = false; //if the update occurs or select return non empty string -> true;
+	  System.out.println("Message received: " + msg + " from " + client);//////////////////////////
+	  String[] msgToStringArr = ((String)msg).split("\\s+");
+	  String firstWord = msgToStringArr[0];
+	  ArrayList<ArrayList<String>> dataToClient = new ArrayList<ArrayList<String>>();
+	  try 
+        {
+		stmt = conn.createStatement();
+		if (firstWord.equals("UPDATE") || firstWord.equals("SET")) {
+			stmt.executeUpdate((String)msg);
+	    	flag = true;
+		}
+		else if  (firstWord.equals("SELECT")) {
+			data = stmt.executeQuery((String)msg);
+			
+			data.toString();
+			int colunmCount = data.getMetaData().getColumnCount();
+    	    while(data.next()) {
+    	    	ArrayList<String> rowTemp= new ArrayList<String>(colunmCount);
+    	    	for (int i = 1; i < colunmCount+1; i++) 
+    	    		rowTemp.add(data.getString(i));
+    	    	dataToClient.add(rowTemp);
+    	    	System.out.println("rowTemp is: " + rowTemp.toString()); //////////////////checks for us
+    	    }
+    	    this.sendToAllClients(dataToClient);
+		}   
+     	} catch (SQLException ex) 
+     	    {/* handle any errors*/
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            this.sendToAllClients("Error");
+     	    }
+  }
+   
+  /**
+   * This method overrides the one in the superclass.  Called
+   * when the server starts listening for connections.
+   */
+  protected void serverStarted()
+  {
+    System.out.println ("Server listening for connections on port " + getPort());
+    if (connectionToDataBase()) 
+    	System.out.println("SQL connection succeed");
+    else
+    	System.out.println("SQL connection fail");
 
-	public void main() {
-		// TODO - implement CEMSserver.main
-		throw new UnsupportedOperationException();
-	}
-
+  }
+  
+  private boolean connectionToDataBase()
+  {
+	 
+	    try 
+		{
+          Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+          System.out.println("Driver definition succeed");
+      } catch (Exception ex) {
+      	/* handle the error*/
+      	 System.out.println("Driver definition failed");
+      	 }
+      try 
+      {
+    	  this.conn  = DriverManager.getConnection("jdbc:mysql://localhost/cems?serverTimezone=IST","root","DorShabat123");
+      } catch (SQLException ex) 
+	    {/* handle any errors*/
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+      return false;
+      }     
+	return true;
+  }
+  
+  
+  /**
+   * This method overrides the one in the superclass.  Called
+   * when the server stops listening for connections.
+   */
+  protected void serverStopped()  {
+    System.out.println ("Server has stopped listening for connections.");
+  }  
 }
+//End of EchoServer class
