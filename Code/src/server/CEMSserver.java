70dec50ae7658139b4826b8679ef81007a6fb7d5
Yuval Rozner <yuvalrozner98@gmail.com>
2023-05-12 11:56:00 +0300

    /

--- Code/src/server/CEMSserver.java
+++ Code/src/server/CEMSserver.java
@@ -1,7 +1,6 @@
 package server;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -91,40 +90,23 @@ public class CEMSserver extends AbstractServer
   protected void serverStarted()
   {
     System.out.println ("Server listening for connections on port " + getPort());
-    if (connectionToDataBase()) 
+    if (new DataBaseConnector().connectionToDataBase(this)) 
     	System.out.println("SQL connection succeed");
     else
     	System.out.println("SQL connection fail");
 
   }
+    
   
-  private boolean connectionToDataBase()
-  {
-	 
-	    try 
-		{
-          Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-          System.out.println("Driver definition succeed");
-      } catch (Exception ex) {
-      	/* handle the error*/
-      	 System.out.println("Driver definition failed");
-      	 }
-      try 
-      {
-    	  this.conn  = DriverManager.getConnection("jdbc:mysql://localhost/cems?serverTimezone=IST","root","DorShabat123");
-    	  //this.conn  = DriverManager.getConnection("jdbc:mysql://localhost/cems?serverTimezone=IST","root","yr13yr13");
-      } catch (SQLException ex) 
-	    {/* handle any errors*/
-      System.out.println("SQLException: " + ex.getMessage());
-      System.out.println("SQLState: " + ex.getSQLState());
-      System.out.println("VendorError: " + ex.getErrorCode());
-      return false;
-      }     
-	return true;
-  }
-  
-  
-  /**
+  public Connection getConn() {
+	return conn;
+}
+
+public void setConn(Connection conn) {
+	this.conn = conn;
+}
+
+/**
    * This method overrides the one in the superclass.  Called
    * when the server stops listening for connections.
    */
--- Code/src/server/DataBaseConnector.java
+++ Code/src/server/DataBaseConnector.java
@@ -1,10 +1,33 @@
 package server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnector {
 
-	public void connectToDataBase() {
-		// TODO - implement DataBaseConnector.connectToDataBase
-		throw new UnsupportedOperationException();
-	}
+	  public boolean connectionToDataBase(CEMSserver Cserver)
+	  {
+		    try 
+			{
+	          Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+	          System.out.println("Driver definition succeed");
+	      } catch (Exception ex) {
+	      	/* handle the error*/
+	      	 System.out.println("Driver definition failed");
+	      	 }
+	      try 
+	      {
+	    	  //conn  = DriverManager.getConnection("jdbc:mysql://localhost/cems?serverTimezone=IST","root","DorShabat123");
+	    	  Cserver.setConn(DriverManager.getConnection("jdbc:mysql://localhost/cems?serverTimezone=IST","root","yr13yr13"));
+	      } catch (SQLException ex) 
+		    {/* handle any errors*/
+	      System.out.println("SQLException: " + ex.getMessage());
+	      System.out.println("SQLState: " + ex.getSQLState());
+	      System.out.println("VendorError: " + ex.getErrorCode());
+	      return false;
+	      }     
+		return true;
+	  }
 
 }
