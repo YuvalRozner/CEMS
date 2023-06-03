90240eb66a0283d4cf529fac3b908c336d10f870
Yuval Rozner <yuvalrozner98@gmail.com>
2023-06-03 18:48:30 +0300

    .

--- Code/src/client/ClientUI.java
+++ Code/src/client/ClientUI.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class ClientUI extends Application {
-	//public static ClientController chat; //only one instance
 	public static ChatClient client;
 
 	public static void main( String args[] ) throws Exception { 
