1371be18f1f2c72ad622986946f1045ae8466fd6
Yuval Rozner <yuvalrozner98@gmail.com>
2023-06-03 12:27:42 +0300

    delete unecessery method from Msg class.

--- Code/src/controllers/JDBC/Msg.java
+++ Code/src/controllers/JDBC/Msg.java
@@ -18,11 +18,6 @@ public class Msg implements Serializable{
 	//private ArrayList<Object> dataToClient;
 	
 	
-	
-	
-	
-	private ArrayList<Object> info = new ArrayList<>();
-	
 	public Msg() {
 	}
 	
@@ -104,59 +99,10 @@ public class Msg implements Serializable{
 	
 	
 	
-	
-	
-	
-	
-	public ArrayList<Object> getInfo() {
-		return info;
-	}
-	public void setInfo(ArrayList<Object> info) {
-		this.info = info;
-	}	
-	
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<String> getSelectInfo() {
-		return (type==MsgType.select)? (ArrayList<String>) info.get(0) : null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<String> getFromInfo() {
-		return (type==MsgType.select)? (ArrayList<String>) info.get(1) : null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<String> getWhereColInfo() {
-		if(type==MsgType.select) return (ArrayList<String>) info.get(2); //in select msg, the whereCol is in index 2
-		if(type==MsgType.update) return (ArrayList<String>) info.get(3); //in update msg, the whereCol is in index 3
-		return null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<Object> getWhereValueInfo() {
-		if(type==MsgType.select) return (ArrayList<Object>) info.get(3); //in select msg, the whereCol is in index 3
-		if(type==MsgType.update) return (ArrayList<Object>) info.get(4); //in update msg, the whereCol is in index 4
-		return null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<String> getTableToUpdateInfo() {
-		return (type==MsgType.update)? (ArrayList<String>) info.get(0) : null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<String> getSetColInfo() {
-		return (type==MsgType.update)? (ArrayList<String>) info.get(1) : null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<Object> getSetValueInfo() {
-		return (type==MsgType.update)? (ArrayList<Object>) info.get(2) : null;
-	}
+
 	
 	@Override
 	public String toString() {
-		return "Msg [type=" + type + ", info=" + info + "]";
+		return "Msg [type=" + type ;
 	}
 }
