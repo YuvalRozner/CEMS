638bec193c0dbe538a81a895f05e6fcc5c5e22c0
Yuval Rozner <yuvalrozner98@gmail.com>
2023-06-07 00:25:34 +0300

    arrange all the client project

---
+++ CEMS_Code/Client/src/gui/AbstractController.java
@@ -0,0 +1,97 @@
+package gui;
+
+import JDBC.Msg;
+import JDBC.MsgType;
+import client.ChatClient;
+import client.ClientUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public abstract class AbstractController implements SceneSetter {
+	private static Stage primaryStage;
+	public static Msg msgReceived;
+	private Scene scene;
+	private String fxmlName;
+	public String prevScreen;
+
+	public void start(String fxmlName, String prevScreen) throws Exception {
+		this.fxmlName = fxmlName;
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlName + ".fxml"));
+		Parent root = loader.load();
+		ChatClient.screens.put(fxmlName, loader.getController());
+		Scene tmpScene = new Scene(root);
+
+		((SceneSetter)loader.getController()).setScene(tmpScene);
+		((SceneSetter)loader.getController()).setPrevScreen(prevScreen);
+
+		primaryStage.setTitle(fxmlName);
+		primaryStage.setScene(tmpScene);
+		primaryStage.centerOnScreen();
+		primaryStage.show();
+	}
+
+	public void setPrevScreen(String prevScreen) {
+		this.prevScreen = prevScreen;
+	}
+
+	public void setScene(Scene scene) {
+		this.scene = scene;
+	}
+
+	public static void sendMsg(Msg msg) {
+		ClientUI.client.handleMessageFromClientUI(msg);
+	}
+
+	public static void setPrimaryStage(Stage primaryStage) {
+		AbstractController.primaryStage = primaryStage;
+	}
+
+	public void display() {
+		primaryStage.setTitle(fxmlName);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public void exitBtn(ActionEvent event) throws Exception {
+		try {
+			logout();
+			sendMsg(new Msg(MsgType.disconnect));
+		} catch (Throwable t) {
+			System.out.println("error getExitBtn");
+			System.exit(0);
+		}
+		System.exit(0); // exit
+	}
+
+	public String getFxmlName() {
+		return fxmlName;
+	}
+	
+	public static Msg getMsgReceived() {
+		return msgReceived;
+	}
+
+	public static void setDataReceived(Msg msgReceived) {
+		AbstractController.msgReceived = msgReceived;
+	}
+	
+	public void backBtn(ActionEvent event) throws Exception {
+		((Node)event.getSource()).getScene().getWindow().hide();
+		ChatClient.getScreen(prevScreen).display();
+	}
+	
+	public void logout() throws Exception {
+		if(ChatClient.user==null) return;
+    	Msg msg = new Msg(MsgType.update);
+		msg.setTableToUpdate("cems.user");
+		msg.setSet("loggedin", "no");
+    	msg.setWhere("username", ChatClient.user.getUsername());
+    	msg.setWhere("password", ChatClient.user.getPassword());
+    	sendMsg(msg);
+	}
+}
---
+++ CEMS_Code/Client/src/gui/ClientConnectionController.java
@@ -0,0 +1,38 @@
+package gui;
+
+import java.io.IOException;
+
+import client.ChatClient;
+import client.ClientUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ClientConnectionController extends AbstractController{
+	
+    @FXML
+    private Button btnConnect;
+    
+    @FXML
+    private TextField inputIp;
+
+    @FXML
+    private TextField inputPort;
+    
+	@FXML
+	private Button btnExit = null;
+	
+    @FXML
+    void connect(ActionEvent event) {
+    	try {
+    		try{ ClientUI.client = new ChatClient(inputIp.getText(), Integer.valueOf(inputPort.getText()));
+    		}catch (IOException exception) { System.out.println("Error: Can't setup connection!"+ " Terminating client.");
+    		      System.exit(1);  }
+    		
+    		start("login", "clientConnection");
+
+    	} catch(Throwable t) {System.out.println("input ip and port - error connecting.");}    	
+    }
+   
+}
