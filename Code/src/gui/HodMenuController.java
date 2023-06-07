13ec5cc71dd980746b28045b9c1c8fdc68914fa0
liorzucker <93440030+liorzucker@users.noreply.github.com>
2023-06-07 11:24:36 +0300

    hod menu

---
+++ Code/src/gui/HodMenuController.java
@@ -0,0 +1,41 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HodMenuController {
+
+    @FXML
+    private Button approve;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Button report;
+
+    @FXML
+    private Button shoeData;
+
+    @FXML
+    void approveChange(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reports(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showData(ActionEvent event) {
+
+    }
+
+}
