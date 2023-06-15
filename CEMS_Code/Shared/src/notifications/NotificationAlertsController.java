0291664cd9376cbcc699e302d7840286bc414df1
Yuval Rozner <yuvalrozner98@gmail.com>
2023-06-15 22:54:52 +0300

    modify the Notifications class so the Alert will work only by the Platform.runLater(() command.

--- CEMS_Code/Shared/src/notifications/NotificationAlertsController.java
+++ CEMS_Code/Shared/src/notifications/NotificationAlertsController.java
@@ -1,10 +1,12 @@
 package notifications;
 
 import java.util.Optional;
+
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-import javafx.scene.control.Alert.AlertType;
 
 /**
  * The NotificationAlertsController class provides methods to show different
@@ -23,32 +25,34 @@ public class NotificationAlertsController {
      * @param label The label to be displayed in the alert.
      */
     public void showInformationAlert(String label) {
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Information Alert");
-        // alert.setHeaderText("Hello"); //set HeaderText we want
-        alert.setHeaderText(null); // without HeaderText
-
-        // Create a label for the content text
-        Label contentLabel = new Label(label);
-        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
-
-        // Set the custom label as the content of the alert
-        alert.getDialogPane().setContent(contentLabel);
-        alert.getDialogPane().setStyle(
-                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
-
-        // Set button styles and add hover effects
-        alert.getDialogPane().getButtonTypes().stream()
-                .map(alert.getDialogPane()::lookupButton)
-                .forEach(button -> {
-                    button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
-                    button.setOnMouseEntered(e -> button.setStyle(
-                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                    button.setOnMouseExited(e -> button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                });
-        alert.showAndWait();
+    	Platform.runLater(() -> {
+	        Alert alert = new Alert(AlertType.INFORMATION);
+	        alert.setTitle("Information Alert");
+	        // alert.setHeaderText("Hello"); //set HeaderText we want
+	        alert.setHeaderText(null); // without HeaderText
+	
+	        // Create a label for the content text
+	        Label contentLabel = new Label(label);
+	        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
+	
+	        // Set the custom label as the content of the alert
+	        alert.getDialogPane().setContent(contentLabel);
+	        alert.getDialogPane().setStyle(
+	                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
+	
+	        // Set button styles and add hover effects
+	        alert.getDialogPane().getButtonTypes().stream()
+	                .map(alert.getDialogPane()::lookupButton)
+	                .forEach(button -> {
+	                    button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
+	                    button.setOnMouseEntered(e -> button.setStyle(
+	                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                    button.setOnMouseExited(e -> button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                }); 
+	        alert.showAndWait();
+    	});
     }
 
     /**
@@ -57,33 +61,35 @@ public class NotificationAlertsController {
      * @param label The label to be displayed in the alert.
      */
     public void showWarningAlert(String label) {
-        Alert alert = new Alert(AlertType.WARNING);
-        alert.setTitle("Warning alert");
-        // alert.setHeaderText("Hello"); //set HeaderText we want
-        alert.setHeaderText(null); // without HeaderText
-
-        // Create a label for the content text
-        Label contentLabel = new Label(label);
-        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
-
-        // Set the custom label as the content of the alert
-        alert.getDialogPane().setContent(contentLabel);
-        alert.getDialogPane().setStyle(
-                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
-
-        // Set button styles and add hover effects
-        alert.getDialogPane().getButtonTypes().stream()
-                .map(alert.getDialogPane()::lookupButton)
-                .forEach(button -> {
-                    button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
-                    button.setOnMouseEntered(e -> button.setStyle(
-                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                    button.setOnMouseExited(e -> button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                });
-
-        alert.showAndWait();
+    	Platform.runLater(() -> {
+	        Alert alert = new Alert(AlertType.WARNING);
+	        alert.setTitle("Warning alert");
+	        // alert.setHeaderText("Hello"); //set HeaderText we want
+	        alert.setHeaderText(null); // without HeaderText
+	
+	        // Create a label for the content text
+	        Label contentLabel = new Label(label);
+	        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
+	
+	        // Set the custom label as the content of the alert
+	        alert.getDialogPane().setContent(contentLabel);
+	        alert.getDialogPane().setStyle(
+	                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
+	
+	        // Set button styles and add hover effects
+	        alert.getDialogPane().getButtonTypes().stream()
+	                .map(alert.getDialogPane()::lookupButton)
+	                .forEach(button -> {
+	                    button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
+	                    button.setOnMouseEntered(e -> button.setStyle(
+	                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                    button.setOnMouseExited(e -> button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                });
+	
+	        alert.showAndWait();
+    	});
     }
 
     /**
@@ -92,34 +98,36 @@ public class NotificationAlertsController {
      * @param label The label to be displayed in the alert.
      */
     public void showErrorAlert(String label) {
-        Alert alert = new Alert(AlertType.ERROR);
-
-        alert.setTitle("Error alert");
-        // alert.setHeaderText("Can not add user"); //set HeaderText we want
-        alert.setHeaderText(null); // without HeaderText
-
-        // Create a label for the content text
-        Label contentLabel = new Label(label);
-        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
-
-        // Set the custom label as the content of the alert
-        alert.getDialogPane().setContent(contentLabel);
-        alert.getDialogPane().setStyle(
-                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
-
-        // Set button styles and add hover effects
-        alert.getDialogPane().getButtonTypes().stream()
-                .map(alert.getDialogPane()::lookupButton)
-                .forEach(button -> {
-                    button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
-                    button.setOnMouseEntered(e -> button.setStyle(
-                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                    button.setOnMouseExited(e -> button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                });
-
-        alert.showAndWait();
+    	Platform.runLater(() -> {
+	        Alert alert = new Alert(AlertType.ERROR);
+	
+	        alert.setTitle("Error alert");
+	        // alert.setHeaderText("Can not add user"); //set HeaderText we want
+	        alert.setHeaderText(null); // without HeaderText
+	
+	        // Create a label for the content text
+	        Label contentLabel = new Label(label);
+	        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
+	
+	        // Set the custom label as the content of the alert
+	        alert.getDialogPane().setContent(contentLabel);
+	        alert.getDialogPane().setStyle(
+	                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
+	
+	        // Set button styles and add hover effects
+	        alert.getDialogPane().getButtonTypes().stream()
+	                .map(alert.getDialogPane()::lookupButton)
+	                .forEach(button -> {
+	                    button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
+	                    button.setOnMouseEntered(e -> button.setStyle(
+	                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                    button.setOnMouseExited(e -> button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                });
+	
+	        alert.showAndWait();
+    	});
     }
 
     /**
@@ -130,48 +138,50 @@ public class NotificationAlertsController {
      * @param headerLabel The header label to be displayed in the alert.
      */
     public void showConfirmationAlert(String label, String headerLabel) {
-        Alert alert = new Alert(AlertType.CONFIRMATION);
-        alert.setTitle("Delete File");
-        alert.setHeaderText(headerLabel);
-
-        // Create a label for the content text
-        Label contentLabel = new Label(label);
-        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
-
-        // Set the custom label as the content of the alert
-        alert.getDialogPane().setContent(contentLabel);
-        alert.getDialogPane().setStyle(
-                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
-
-        // Style the header text
-        alert.getDialogPane().lookup(".header-panel").setStyle(
-                "-fx-background-color: #92bce3; -fx-background-radius: 10px; -fx-text-fill: white; -fx-font-weight: bold; -fx-font-family: 'Comic Sans MS';");
-
-        // Set button styles and add hover effects
-        alert.getDialogPane().getButtonTypes().stream()
-                .map(alert.getDialogPane()::lookupButton)
-                .forEach(button -> {
-                    button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
-                    button.setOnMouseEntered(e -> button.setStyle(
-                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                    button.setOnMouseExited(e -> button.setStyle(
-                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
-                });
-
-        Optional<ButtonType> option = alert.showAndWait();
-
-        if (option.isPresent()) {
-            if (option.get() == ButtonType.OK) {
-                if (onOkAction != null) {
-                    onOkAction.run();
-                }
-            } else if (option.get() == ButtonType.CANCEL) {
-                if (onCancelAction != null) {
-                    onCancelAction.run();
-                }
-            }
-        }
+    	Platform.runLater(() -> {
+	        Alert alert = new Alert(AlertType.CONFIRMATION);
+	        alert.setTitle("Delete File");
+	        alert.setHeaderText(headerLabel);
+	
+	        // Create a label for the content text
+	        Label contentLabel = new Label(label);
+	        contentLabel.setStyle("-fx-font-weight: bold; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 14px;");
+	
+	        // Set the custom label as the content of the alert
+	        alert.getDialogPane().setContent(contentLabel);
+	        alert.getDialogPane().setStyle(
+	                "-fx-background-color: #F8FFFF; -fx-border-color: #92bce3; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-padding: 10px;");
+	
+	        // Style the header text
+	        alert.getDialogPane().lookup(".header-panel").setStyle(
+	                "-fx-background-color: #92bce3; -fx-background-radius: 10px; -fx-text-fill: white; -fx-font-weight: bold; -fx-font-family: 'Comic Sans MS';");
+	
+	        // Set button styles and add hover effects
+	        alert.getDialogPane().getButtonTypes().stream()
+	                .map(alert.getDialogPane()::lookupButton)
+	                .forEach(button -> {
+	                    button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";");
+	                    button.setOnMouseEntered(e -> button.setStyle(
+	                            "-fx-background-color: #4096EE; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                    button.setOnMouseExited(e -> button.setStyle(
+	                            "-fx-background-color: #92bce3; -fx-text-fill: white; -fx-font-weight: bold; -fx-background-radius: 7px; -fx-font-family: \"Comic Sans MS\";"));
+	                });
+	
+	        Optional<ButtonType> option = alert.showAndWait();
+	
+	        if (option.isPresent()) {
+	            if (option.get() == ButtonType.OK) {
+	                if (onOkAction != null) {
+	                    onOkAction.run();
+	                }
+	            } else if (option.get() == ButtonType.CANCEL) {
+	                if (onCancelAction != null) {
+	                    onCancelAction.run();
+	                }
+	            }
+	        }
+    	});
     }
 
     /**
