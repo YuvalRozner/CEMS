87854554176ab08dabfb3ca53f9f6a886d83015b
Yuval Rozner <yuvalrozner98@gmail.com>
2023-06-23 13:40:26 +0300

    Update MsgTest.java

--- CEMS_Code/Shared/UnitTest/JDBC/MsgTest.java
+++ CEMS_Code/Shared/UnitTest/JDBC/MsgTest.java
@@ -1,6 +1,8 @@
 package JDBC;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 
@@ -20,6 +22,7 @@ class MsgTest {
 	private static String str2;
 	private static ArrayList<Object> tmpLst;
 	private static ArrayList<MyObject> expected;
+	private static ArrayList<MyObject> result;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -29,7 +32,6 @@ class MsgTest {
 		int2 = 80;
 		str1 = new String("name1");
 		str2 = new String("name2");
-		
 	}
 	
 	@BeforeEach
@@ -37,11 +39,13 @@ class MsgTest {
 		msgBeingTested = new Msg(MsgType.data);
 		msgBeingTested.setData(data);
 		expected = new ArrayList<MyObject>();
+		result = null;
 	}
 	
     // Define the DataObject class for testing on
     static class MyObject {
-        private Integer intVal;
+
+		private Integer intVal;
         private String stringVal;
         
         // constructor 1:
@@ -64,20 +68,145 @@ class MsgTest {
             this.stringVal = stringVal;
         }
         
+        // equals method:
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null) return false;
+			if (getClass() != obj.getClass()) return false;
+			MyObject mo = (MyObject) obj;
+			return (  (intVal.equals(mo.intVal)) && (stringVal.equals(mo.stringVal))  );
+		}
+        
         // toString method:
         public String toString() {
         	return "MyObject["+intVal.toString()+", "+stringVal+"]";
-        }
+        }        
     }
     
+    // Description: Test conversion when there is a matching constructor.
+    // Input: data = {MyObject= {50, "name1"}}.
+    // Expected Result: list in size of one of MyObject with the value of the input.
+    @Test
+    void ConvertDataTest_OneMatchingConstructor() {
+        data = new ArrayList<ArrayList<Object>>();
+        tmpLst = new ArrayList<Object>();
+        tmpLst.add(int1);
+        tmpLst.add(str1);
+        data.add(tmpLst);
+        expected.add(myObj1);
+        msgBeingTested.setData(data);
+        result = msgBeingTested.convertData(MyObject.class);
+        assertEquals(expected, result);
+    }
     
-    // Description: 
-    // Input: 
-    // Expected Result:
-	@Test
-	void test() {
-		//ArrayList<Object>
-		assertTrue(true);
-	}
+    // Description: Test conversion with multiple data rows.
+    // Input: list of list {{int1, str1}, {int2, str2}}.
+    // Expected Result: list of MyObject {{int1, str1}, {int2, str2}}.
+    @Test
+    void ConvertDataTest_MultipleDataRows() {
+        data = new ArrayList<ArrayList<Object>>();
+        tmpLst = new ArrayList<Object>();
+        tmpLst.add(int1);
+        tmpLst.add(str1);
+        data.add(tmpLst);
+        tmpLst = new ArrayList<Object>();
+        tmpLst.add(int2);
+        tmpLst.add(str2);
+        data.add(tmpLst);
+        expected.add(myObj1);
+        expected.add(myObj2);
+        msgBeingTested.setData(data);
+        ArrayList<MyObject> result = msgBeingTested.convertData(MyObject.class);
+        assertEquals(expected, result);
+    }
+	
+    // Description: Test conversion of empty data list.
+    // Input: msgBeingTested{type=data, empty data list}.
+    // Expected Result: empty list of MyObject.
+    @Test
+    void ConvertDataTest_EmptyDataList() {
+        data = new ArrayList<ArrayList<Object>>();
+        msgBeingTested.setData(data);
+        try{ result = msgBeingTested.convertData(MyObject.class);
+        }catch(Exception e) {fail("exception.");}
+        assertEquals(0, result.size());
+    }
 
+    // Description: Test conversion with multiple matching constructors.
+    // Input: list {integer 50}.
+    // Expected Result: same list as input.
+    @Test
+    void ConvertDataTest_MultipleMatchingConstructors() {
+        data = new ArrayList<ArrayList<Object>>();
+        tmpLst = new ArrayList<Object>();
+        tmpLst.add(int1);
+        data.add(tmpLst);
+        expected.add(new MyObject(int1));
+        msgBeingTested.setData(data);
+        try{ result = msgBeingTested.convertData(MyObject.class);
+        assertEquals(expected, result);
+        }catch(Exception e) {fail("exception.");}
+        assertTrue(true);
+    }
+    
+    // Description: Test conversion with no matching constructors.
+    // Input: list {String "name1"}.
+    // Expected Result: empty list.
+    @Test
+    void ConvertDataTest_NoMatchingConstructor() {
+        data = new ArrayList<ArrayList<Object>>();
+        tmpLst = new ArrayList<Object>();
+        tmpLst.add(str1);
+        data.add(tmpLst);
+        msgBeingTested.setData(data);
+        result = msgBeingTested.convertData(MyObject.class);
+        assertEquals(0, result.size());
+    }
+    
+    // Description: Test conversion with invalid data type in data row.
+    // Input: Invalid data type Integer list.
+    // Expected Result: empty list.
+    @Test
+    void ConvertDataTest_InvalidDataType() {
+        data = new ArrayList<ArrayList<Object>>();
+        tmpLst = new ArrayList<Object>();
+        tmpLst.add("invalid"); // Invalid data type, should be Integer
+        data.add(tmpLst);
+        msgBeingTested.setData(data);
+        result = msgBeingTested.convertData(MyObject.class);
+        assertEquals(0, result.size());
+    }
+    
+    // Description: Test conversion with null data row.
+    // Input: data list of null list.
+    // Expected Result: empty list.
+    @Test
+    void ConvertDataTest_NullDataRow() {
+    	try{
+        data = new ArrayList<ArrayList<Object>>();
+        ArrayList<Object> tmp = new ArrayList<Object>();
+        data.add(tmp);
+        msgBeingTested.setData(data);
+        result = msgBeingTested.convertData(MyObject.class);
+        assertEquals(0, result.size());
+        }catch(Exception e) {fail("exception.");}
+        assertTrue(true);
+    }
+    
+    // Description: Test conversion with null parameter types.
+    // Input: list of list{MyObject{int1, str1}}, use the method on general Object type.
+    // Expected Result:empty list
+    @Test
+    void ConvertDataTest_NullParameterTypes() {
+        data = new ArrayList<ArrayList<Object>>();
+        tmpLst = new ArrayList<Object>();
+        tmpLst.add(int1);
+        tmpLst.add(str1);
+        data.add(tmpLst);
+        msgBeingTested.setData(data);
+        try{ ArrayList<Object> result = msgBeingTested.convertData(Object.class);
+        assertEquals(0, result.size());
+        }catch(Exception e) {fail("exception.");} 
+        assertTrue(true);
+    }
 }
